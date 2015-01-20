58bace8a54de94b7591a1b42df05fbbb76aca7dc
Denis Laliberté <denis@tp1.ca>
2015-01-20 12:42:30 -0500

    filter one element list

--- spec/QuickSort/core_spec.clj
+++ spec/QuickSort/core_spec.clj
@@ -5,4 +5,9 @@
 (describe "Quicksort"
   (it "return an empty list"
     (should= '() (quicksort '())))
+  (it "return one element array"
+    (should= '(1) (quicksort '(1))))
+  (it "sort three element array"
+  (pending)
+    (should= '(3,2,1) (quicksort '(1,2,3))))
 )
--- src/QuickSort/core.clj
+++ src/QuickSort/core.clj
@@ -1,6 +1,6 @@
 (ns quicksort.core)
 
 (defn quicksort "it return a list sorted" [a]
-  '()
+  a
 )
 
